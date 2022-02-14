ec9c489242ea587db79de00fa451b4383ae92dc6
sivagamasheridan <46792094+sivagamasheridan@users.noreply.github.com>
2022-02-13 21:57:36 -0500

    Update ArithmeticBase.java

--- src/arithmetic/ArithmeticBase.java
+++ src/arithmetic/ArithmeticBase.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
  * from user and execute the arithmetic operation
  * change the code to use enum instead String and mention the advantage of enum.
  * @author sivagamasrinivasan
- * date 07/05/21
+ * date 14/02/22
  */
 public class ArithmeticBase 
 {
